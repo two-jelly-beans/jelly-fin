5e97a6b9f03e2a69a0a5726d113d7d91ae8c455d
Jonathan Irvin <jonathanirvin@cassiopeia.lan>
2018-08-06 21:54:12 -0500

    feat: scaffold out transaction service

---
+++ api/transactions/transactions/create.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.text !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the transaction',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      date: data.date,
+      category: data.category,
+      paymentMethod: data.paymentMethod,
+      payee: data.payee,
+      note: data.note,
+      amount: data.amount,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the transaction.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
