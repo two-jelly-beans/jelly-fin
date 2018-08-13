e342b11c1c04b379a64a7673895e7f3ec2334f92
Jonathan Irvin <djfoxyslpr@gmail.com>
2018-08-12 22:02:33 -0500

    fix: trying to find out why the object isn't getting mapped

--- api/transactions/transactions/create.js
+++ api/transactions/transactions/create.js
@@ -1,56 +1,70 @@
-'use strict';
+"use strict";
 
-const uuid = require('uuid');
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const uuid = require("uuid");
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
-  const data = JSON.parse(event.body);
-  if (typeof data.text !== 'string') {
-    // console.error('Validation Failed');
-    callback(null, {
-      statusCode: 400,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Couldn\'t create the transaction',
-    });
-    return;
-  }
 
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: {
-      id: uuid.v1(),
-      date: data.date,
-      category: data.category,
-      paymentMethod: data.paymentMethod,
-      payee: data.payee,
-      note: data.note,
-      amount: data.amount,
-      createdAt: timestamp,
-      updatedAt: timestamp
-    },
-  };
-
-  // write the todo to the database
-  dynamoDb.put(params, (error) => {
-    // handle potential errors
-    if (error) {
-      // console.error(error);
+  if (typeof event.body !== "undefined") {
+    const data = JSON.parse(event.body);
+    if (typeof data.amount !== "number") {
+      console.error("Validation Failed");
       callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the transaction.',
+        statusCode: 400,
+        headers: { "Content-Type": "text/plain" },
+        body:
+          "Validation error. Couldn't create the transaction " +
+          JSON.stringify(data)
       });
       return;
     }
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1(),
+        date: data.date,
+        category: data.category,
+        paymentMethod: data.paymentMethod,
+        payee: data.payee,
+        note: data.note,
+        amount: data.amount,
+        createdAt: timestamp,
+        updatedAt: timestamp
+      }
     };
-    callback(null, response);
-  });
+
+    // write the todo to the database
+    dynamoDb.put(params, (error) => {
+      // handle potential errors
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { "Content-Type": "text/plain" },
+          body:
+            "Couldn't create the transaction.  Database write failed. Error: " +
+            error
+        });
+        return;
+      }
+
+      // create a response
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(params.Item)
+      };
+      callback(null, response);
+    });
+  } else {
+    callback(null, {
+      statusCode: 400,
+      headers: { "Content-Type": "text/plain" },
+      body: "Invalid body " + JSON.stringify(event, null, 2)
+    });
+    return;
+  }
 };
