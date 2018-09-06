9828431ff30ed4c5cca5182ebbe327fcfcabfcf7
Jonathan Irvin <djfoxyslpr@gmail.com>
2018-09-05 23:03:44 -0500

    hack: testing react native :)

--- App.js
+++ App.js
@@ -5,9 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Welcome to Jelly Fin!</Text>
       </View>
     );
   }
