d0fec9ebec2eb19939a485574c17bd24fdcb7034
Jonathan Irvin <jonathan.irvin@uicentric.com>
2018-09-10 10:59:35 -0500

    Revert "chore: cleanup prior to React Native switch"
    This reverts commit 49f66afcb47ca402d125d189d5cb617c7e23696c.

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+// exports.addTransaction = functions.https.onRequest((req, res) => {
+//   const timestamp = new Date().getTime()
+//   const data = req.body
+//   return admin.database().ref('/transactions').push({
+//     date: data.date,
+//     category: data.category,
+//     paymentMethod: data.paymentMethod,
+//     payee: data.payee,
+//     note: data.note,
+//     amount: data.amount,
+//     createdAt: timestamp,
+//     updatedAt: timestamp
+//   }).then((snapshot) => {
+//     return res.redirect(303, snapshot.ref.toString());
+//   });
+// });
