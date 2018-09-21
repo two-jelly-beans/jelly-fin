1c8ca2ddef8fd2ad638c5ed95da6935df47fabc8
Mohammed Salman <msal4@outlook.com>
2018-09-21 15:56:34 +0300

    fixed firebase config import

--- src/firebase-app.js
+++ src/firebase-app.js
@@ -1,11 +1,17 @@
 import firebase from '@firebase/app';
 import 'firebase/firestore';
 
+// Imports a module without throwing and error
+const tryRequire = (path) => {
+    try {
+        return require(`${path}`);
+    } catch (err) {
+        return null;
+    }
+};
+
 // A config file that contains your firebase project credentials (not included in the repo)
-import FIREBASE_CONFIG from './firebase-config.json';
-
-
-const CONFIG = FIREBASE_CONFIG || {
+const CONFIG = tryRequire('./firebase-config.json') || {
     apiKey: "AIzaSyAcq_Vr-wbCjctpWIXJdeXBHnQgSqCLRY8",
     authDomain: "jellyfin-a9ff6.firebaseapp.com",
     databaseURL: "https://jellyfin-a9ff6.firebaseio.com",
@@ -13,6 +19,7 @@ const CONFIG = FIREBASE_CONFIG || {
     storageBucket: "",
     messagingSenderId: "505439361090"
 };
+
 
 // Initialize Cloud Firestore through Firebase
 const app = firebase.initializeApp(CONFIG);
@@ -26,7 +33,7 @@ db.settings({
 
 // Add a document to a collection
 db.collection("test-collection").add({
-    title: 'This is the post title',
+    title: 'This is the post title...',
     content: 'This is the test post content. It should be longeeer...',
     date: new Date(),
 })
