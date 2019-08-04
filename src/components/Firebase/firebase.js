84c4c3ebd4cb84b2a5d0a9e3b729617099f1cb9c
Jonathan Irvin <djfoxyslpr@gmail.com>
2019-08-03 21:37:38 -0500

    fix(firebase): add app id

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -9,6 +9,7 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 class Firebase {
