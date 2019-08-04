ae016d9ac7d5a6cb30995ad4303b4c78e9dd2067
Jonathan Irvin <djfoxyslpr@gmail.com>
2019-08-03 22:29:39 -0500

    fix(messages): add messages back in

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -98,6 +98,12 @@ class Firebase {
   user = uid => this.db.doc(`users/${uid}`);
 
   users = () => this.db.collection('users');
+
+  // *** Message API ***
+
+  message = uid => this.db.doc(`messages/${uid}`);
+
+  messages = () => this.db.collection('messages');
 }
 
 export default Firebase;
